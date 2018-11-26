e134c7dcb714609182b369cbeb2c63a15c55df1e
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-11-26 04:39:35 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def build_network(net_call_letters)
-    self.network_id = Network.new(call_letters: net_call_letters)
+    self.network_id = Network.new(call_letters: net_call_letters.values)
     binding.pry
   end
 end

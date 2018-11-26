d52184ae63b329d895560d67875f5a55dcb6fc7d
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-11-26 04:29:58 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,8 +1,10 @@
+require 'pry'
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
   def build_network(net_call_letters)
     self.network_id = Network.new(call_letters: net_call_letters)
+    binding.pry
   end
 end

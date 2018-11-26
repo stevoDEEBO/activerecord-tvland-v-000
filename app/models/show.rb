f1c79dc3c164561479ed0d4a5b500e67b863a7ef
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-11-26 04:11:14 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
-  def build_network(network_id)
+  def build_network(network)
     network_id.call_letters
   end
 end

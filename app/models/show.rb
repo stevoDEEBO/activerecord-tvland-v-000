24e76da20a23b5c5a6d6376762767f7c8ff8a38f
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-11-26 04:49:52 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
   belongs_to :network
 
   def build_network(net_call_letters)
-    self.network = Network.new(call_letters: net_call_letters.defaults)
+    self.network = Network.new(call_letters: net_call_letters.default)
     #binding.pry
   end
 end
